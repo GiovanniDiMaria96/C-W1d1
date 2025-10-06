14015c3ccc8016c33ed7f87e95a526a7ddf0e95c
GiovanniDiMaria <158842385+GiovanniDiMaria96@users.noreply.github.com>
2025-10-06 12:39:59 +0200

    Update Rettangolo.cs

--- 3/3/1. Try, catch, finally/Try_catch_finally/Try_catch_finally/Rettangolo.cs
+++ 3/3/1. Try, catch, finally/Try_catch_finally/Try_catch_finally/Rettangolo.cs
@@ -16,3 +16,30 @@ namespace Try_catch_finally
         }
     }
 }
+
+
+
+// PROPRIETA'
+//modificatoreDiAccesso (SEGUITO DA) Tipo (SEGUITO DA) NomeProp { get; set; } (set per impostarlo e il get per restituire il valore)
+// es.
+// public int Altezza { get; set; }
+// public string Altezza { get; set; }
+// public float Altezza { get; set; }
+//ecc..
+//costruttore (metodo che non da ritorno, non obbligatorio)
+//costruttore default
+// public Rettangolo () { }
+//overloading del costruttore (inserendo proprietà non di default)
+// public Rettangolo (double h, double b)
+// {
+//      this.Altezza = h;
+// }
+// metodi
+//AccessModifier (SEGUITO DA) TipoDaRestituire (SEGUITO DA) NomeMetodo([eventuali parametri])
+//{
+//
+//}
+//es
+// public void Benvenuto(){              ***(VOID NON RESTITUISCE NULLA, MA ESEGUE LE ISTRUZIONI DEL METODO)***
+//  Console.WriteLine("Benvenuto!")
+// }

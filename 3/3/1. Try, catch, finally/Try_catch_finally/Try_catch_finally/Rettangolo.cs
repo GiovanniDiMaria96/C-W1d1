c8257aacb7217a37cc26f6589d0b8afd1be024f2
GiovanniDiMaria <158842385+GiovanniDiMaria96@users.noreply.github.com>
2025-10-06 12:14:02 +0200

    Add files via upload

---
+++ 3/3/1. Try, catch, finally/Try_catch_finally/Try_catch_finally/Rettangolo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Try_catch_finally
+{
+    public class Rettangolo
+    {
+        public int Base { get; set; }
+        public int Altezza { get; set; }
+        public int Area()
+        {
+            return Base * Altezza / 2;
+        }
+    }
+}

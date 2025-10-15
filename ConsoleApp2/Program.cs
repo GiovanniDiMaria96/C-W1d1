bab738ef3c1bd515cf4d7c808572d87e45261757
GiovanniDiMaria <158842385+GiovanniDiMaria96@users.noreply.github.com>
2025-10-15 11:59:01 +0200

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string testo = "uno due tre due tre tre";
+            string[] split = testo.Split(" ");
+            Dictionary<string, int> map = new Dictionary<string, int>();
+
+            foreach (var item in split)
+            {
+                if (map.ContainsKey(item))
+                {
+                    map[item] += 1;
+                }
+                else
+                {
+                    map.Add(item, 1);
+                }
+            }
+            foreach (var item in map)
+            {
+                Console.WriteLine(item.Key + item.Value);
+            }
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> regali = new List<string>();
+            regali.Add("regalo1");
+            regali.Add("regalo2");
+            regali.Add("regalo3");
+            regali.Add("regalo4");
+
+            foreach (var item in regali)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
